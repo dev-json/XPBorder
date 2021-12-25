e81321bfe01f7b2951d8f96ec6f99171601c32a1
JXCoding <hsvbuh12@gmail.com>
2021-12-25 12:47:36 +0100

    new methods for better usages, also optimized old code

--- XPBorder/src/main/java/de/jxson/xpborder/world/worldborder/WorldborderManager.java
+++ XPBorder/src/main/java/de/jxson/xpborder/world/worldborder/WorldborderManager.java
@@ -18,6 +18,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Author: Jason M.
@@ -28,14 +29,20 @@ public class WorldborderManager {
 
     private I_XPBorderManager i_xpBorderManager;
     private ConfigManager worldborderConfig;
-    private SettingsManager settingsManager = XPBorder.getInstance().getSettingsManager();
+    private SettingsManager settingsManager;
 
-    private int increaseSize, currentSize, level;
+    private int increaseSize;
+    private int currentSize;
+    private int level;
+
+    private int sizeInBlocks;
+
     float expbar;
 
     public WorldborderManager() {
         worldborderConfig = new ConfigManager("worldborder.yml");
         i_xpBorderManager = XPBorder.getInstance().getXPBorderManager();
+        settingsManager = XPBorder.getInstance().getSettingsManager();
     }
 
     public void initializeBorder(Player player) {
@@ -53,6 +60,11 @@ public class WorldborderManager {
             worldborderConfig.set("worldborder."+player.getWorld().getName()+".center.world", player.getLocation().getWorld().getName());
         if(worldborderConfig.getString("worldborder.level") == null)
             worldborderConfig.set("worldborder.level", 0);
+        if(worldborderConfig.getString("worldborder.size") == null) {
+            if(worldborderConfig.getString("worldborder.level") == null)
+                worldborderConfig.set("worldborder.level", 0);
+            worldborderConfig.set("worldborder.size", worldborderConfig.getInt("worldborder.level"));
+        }
         if(worldborderConfig.getString("worldborder.xpbar") == null)
             worldborderConfig.set("worldborder.xpbar", 0.0);
     }
@@ -63,13 +75,18 @@ public class WorldborderManager {
         if(!settingsManager.getSetting("xpborder").isToggled())
             return;
 
+
         double cX = worldborderConfig.getDouble("worldborder."+player.getWorld().getName()+".center.x");
         double cZ = worldborderConfig.getDouble("worldborder."+player.getWorld().getName()+".center.z");
-        this.currentSize = worldborderConfig.getInt("worldborder.level");
+        this.currentSize = worldborderConfig.getInt("worldborder.size");
         this.increaseSize = Integer.parseInt(settingsManager.getSetting("expandsize").value());
 
         i_xpBorderManager.setWorld(player.getWorld());
         i_xpBorderManager.setCenter(cX, cZ);
+
+        if(settingsManager.getSetting("calctype").value().equals(BorderSizeCalculationType.ADD.name())) {
+            setSizeInBlocks(Bukkit.getOnlinePlayers().stream().mapToInt(Player::getLevel).sum());
+        }
 
         for(int i = 0; i < currentSize; i++) {
             if(MathUtils.isEven(i)) {
@@ -102,18 +119,28 @@ public class WorldborderManager {
 
         if(!settingsManager.getSetting("xpborder").isToggled())
             return;
+        int a = getSizeInBlocks();
 
-        int old = worldborderConfig.getInt("worldborder.level");
+        if(settingsManager.getSetting("shrink").isToggled() || worldborderConfig.getInt("worldborder.size") < getSizeInBlocks() || settingsManager.getSetting("calctype").value().equals(BorderSizeCalculationType.ADD.name())) {
+            if(settingsManager.getSetting("calctype").value().equals(BorderSizeCalculationType.CONFIG.name())) {
+                setSizeInBlocks(player.getLevel());
+            } else {
+                setSizeInBlocks(Bukkit.getOnlinePlayers().stream().mapToInt(Player::getLevel).sum());
+            }
+        }
+
+        int old = worldborderConfig.getInt("worldborder.size");
 
         setLevel(player.getLevel());
 
         i_xpBorderManager.setWorld(player.getWorld());
-        this.currentSize = worldborderConfig.getInt("worldborder.level");
+        this.currentSize = worldborderConfig.getInt("worldborder.size");
         this.increaseSize = Integer.parseInt(settingsManager.getSetting("expandsize").value());
 
         //prevent this from setting the size to 0
         if (this.increaseSize <= 0)
             this.increaseSize = 1;
+
 
         for (int i = 0; i < currentSize; i++) {
             if (MathUtils.isEven(i)) {
@@ -121,12 +148,13 @@ public class WorldborderManager {
             }
         }
 
-
-
-        if (currentSize == 0 || currentSize == 1)
-            setAdjustSize(old, currentSize + 1);
-        else
-            setAdjustSize(old * increaseSize, (currentSize * increaseSize) + 1);
+        if(settingsManager.getSetting("shrink").isToggled() || a < getSizeInBlocks()) {
+            if (currentSize == 0 || currentSize == 1) {
+                setAdjustSize(old, currentSize + 1);
+            } else {
+                setAdjustSize(old * increaseSize, (currentSize * increaseSize) + 1);
+            }
+        }
     }
 
     private void setAdjustSize(int from, int to) {
@@ -162,25 +190,35 @@ public class WorldborderManager {
         boolean isToggled = settingsManager.getSetting("death").isToggled();
         int calcedLevel = (int) (level*(percent/100f));
         if(isToggled) {
-            setLevel(calcedLevel);
             return calcedLevel;
         } else {
             return 0;
         }
     }
 
-    //Rework this method, because it dont work sometimes
+    public int calcRespawnLevelFromPlayer(Player player) {
+        int level = player.getLevel();
+        int percent = Integer.parseInt(settingsManager.getSetting("death").value());
+        boolean isToggled = settingsManager.getSetting("death").isToggled();
+        int calcedLevel = (int) (level*(percent/100f));
+        if(isToggled) {
+            return calcedLevel;
+        } else {
+            return 0;
+        }
+
+
+    }
+
     public void checkIfPlayerIsOutside(Player player) {
         if(settingsManager.getSetting("security").isToggled()) {
-            if (!player.hasPermission("xpb.bypass")) {
-                new IsPlayerInBorderTask(player).runTaskTimer(XPBorder.getInstance(), 60, 20);
-            }
+            new IsPlayerInBorderTask(player).runTaskTimer(XPBorder.getInstance(), 60, 20);
         }
     }
 
     public void createBossbar(Player player) {
         if(settingsManager.getSetting("bossbar").isToggled()) {
-            BossBar bossBar = Bukkit.createBossBar(Data.color("&a&l" + getLevel()), BarColor.GREEN, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG);
+            BossBar bossBar = Bukkit.createBossBar(Data.color("&a&l" + player.getLevel()), BarColor.GREEN, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG);
             bossBar.addPlayer(player);
             bossBar.setProgress(1);
             bossBar.setVisible(true);
@@ -205,7 +243,12 @@ public class WorldborderManager {
     }
 
     public void setLevel(int level) {
-        worldborderConfig.set("worldborder.level", level);
+
+        String type = settingsManager.getSetting("calctype").value();
+
+        if(BorderSizeCalculationType.valueOf(type) == BorderSizeCalculationType.CONFIG) {
+            worldborderConfig.set("worldborder.level", level);
+        }
         this.level = level;
 
     }
@@ -213,6 +256,17 @@ public class WorldborderManager {
     public int getLevel() {
         level = worldborderConfig.getInt("worldborder.level");
         return level;
+    }
+
+    public void setSizeInBlocks(int size) {
+        worldborderConfig.set("worldborder.size", size);
+        this.sizeInBlocks = size;
+
+    }
+
+    public int getSizeInBlocks() {
+        sizeInBlocks = worldborderConfig.getInt("worldborder.size");
+        return sizeInBlocks;
     }
 
     public void setExpbar(float expbar) {
@@ -228,7 +282,7 @@ public class WorldborderManager {
     public void reload() {
         try {
             worldborderConfig.getConfiguration().load(worldborderConfig.getFile());
-
+            settingsManager.initSettings();
         } catch (IOException | InvalidConfigurationException exception) {
             exception.printStackTrace();
         }
