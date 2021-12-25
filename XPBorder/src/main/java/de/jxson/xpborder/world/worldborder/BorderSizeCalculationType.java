270e68eea6eb88dca95b27ce9225d3eeaa63c246
JXCoding <hsvbuh12@gmail.com>
2021-12-25 12:43:15 +0100

    added new enums

---
+++ XPBorder/src/main/java/de/jxson/xpborder/world/worldborder/BorderSizeCalculationType.java
@@ -0,0 +1,13 @@
+package de.jxson.xpborder.world.worldborder;
+
+/**
+ * Author: Jason M.
+ * de.jxson.xpborder.world.worldborder
+ * 25.12.2021 at 05:27
+ */
+public enum BorderSizeCalculationType {
+
+    CONFIG,
+    ADD;
+
+}
